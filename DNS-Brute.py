03ffca5f3f37bf7e87a8c004297ba313c86fd1c2
mousavil <amirmousavi@mail.um.ac.ir>
2022-02-23 10:42:10 +0330

    check

--- DNS-Brute.py
+++ DNS-Brute.py
@@ -215,10 +215,11 @@ async def main():
     #     # shuffledns-step1
         try:
             response = subprocess.Popen(
-                ['echo',"'www.pishrocloud.com\nsupport.pishrocloud.com'",'|','shuffledns', '-d', domain.name , '-r' ,'resolvers.txt', '-silent'], stdin=subprocess.PIPE,stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True )
-            # inputs=' \n '.join(merged_subdomains_and_wlist).encode('utf8')
-            # response.stdin.write(inputs)
+                ['shuffledns', '-d', domain.name , '-r' ,'resolvers.txt', '-silent'], stdin=subprocess.PIPE,stdout=subprocess.PIPE, stderr=subprocess.PIPE )
+            inputs=' \n '.join(merged_subdomains_and_wlist).encode('utf8')
+            response.stdin.write(inputs)
             out,err=response.communicate()
+            response.stdin.close()
             output=out.decode('utf-8')
             print(output,err)
         except Exception as e:
